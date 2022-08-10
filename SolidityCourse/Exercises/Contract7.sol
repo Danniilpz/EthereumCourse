4753215f948452da03d76992ce255154a949737c
Daniel López Marqués <danilopez99d@gmail.com>
2022-08-10 22:51:15 +0200

    Session 10/08/2022

--- SolidityCourse/Exercises/Contract7.sol
+++ SolidityCourse/Exercises/Contract7.sol
@@ -1,4 +1,6 @@
- pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
+
+//Security Token
 
 contract MyToken{
     string public name;
@@ -7,6 +9,27 @@ contract MyToken{
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
+
+    uint256 dividendPerToken;
+    mapping(address => uint256) dividendBalanceOf;
+    mapping(address => uint256) dividendCreditedTo;
+
+    function update(address _address) internal {
+        uint256 debit=dividendPerToken-dividendCreditedTo[_address];
+        dividendBalanceOf[_address]+=balanceOf[_address]*debit;
+        dividendCreditedTo[_address]=dividendPerToken; 
+    }
+
+    function withdraw() public{
+        update(msg.sender);
+        uint256 amount=dividendBalanceOf[msg.sender];
+        dividendBalanceOf[msg.sender]=0;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function deposit() public payable{
+        dividendPerToken= msg.value /totalSupply;
+    }
 
     constructor(){
         name="My Token";
@@ -21,6 +44,8 @@ contract MyToken{
 
     function transfer(address _to, uint256 _value) public returns (bool success){
         require(balanceOf[msg.sender] >= _value);
+        update(msg.sender);
+        update(_to);
         balanceOf[msg.sender]-=_value;
         balanceOf[_to]+=_value;
         emit Transfer(msg.sender,_to,_value);
